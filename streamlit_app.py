98ead36aae21813df9a1d94f39a3c40352595753
Maxim <maxim.oweyssi@est.org.uk>
2023-01-04 16:28:35 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,23 +2,23 @@ import streamlit as st
 import streamlit_authenticator as stauth
 import yaml
 from yaml import SafeLoader
-
+st.write("helloworld")
 #hashed_passwords = stauth.Hasher(['123', '456']).generate()
 #print(hashed_passwords[0])
 #print(hashed_passwords[1])
 
-with open('../config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
-
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
-name, authentication_status, username = authenticator.login('Login', 'main')
-
+#with open('../config.yaml') as file:
+#    config = yaml.load(file, Loader=SafeLoader)
+#
+#authenticator = stauth.Authenticate(
+#    config['credentials'],
+#    config['cookie']['name'],
+#    config['cookie']['key'],
+#    config['cookie']['expiry_days'],
+#    config['preauthorized']
+#)
+#name, authentication_status, username = authenticator.login('Login', 'main')
+#
 #if authentication_status:
 #    authenticator.logout('Logout', 'main')
 #    st.write(f'Welcome *{name}*')
