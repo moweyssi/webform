582355874af92eaf3aea12f1b03f938ee1142a33
Maxim <maxim.oweyssi@est.org.uk>
2023-01-04 17:16:16 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,5 @@ try:
         st.success('User registered successfully')
 except Exception as e:
     st.error(e)
+with open('config.yaml', 'w') as file:
+    yaml.dump(config, file, default_flow_style=False)
