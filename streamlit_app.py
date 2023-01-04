0f65e2d9e93c28674a1cae371f3f9b53c7b43eb5
Maxim <maxim.oweyssi@est.org.uk>
2023-01-04 17:00:09 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,22 @@
 import streamlit as st
-#import streamlit_authenticator as stauth
-#import yaml
-#from yaml import SafeLoader
-st.write("helloworld")
+import streamlit_authenticator as stauth
+import yaml
+from yaml import SafeLoader
 #hashed_passwords = stauth.Hasher(['123', '456']).generate()
 #print(hashed_passwords[0])
 #print(hashed_passwords[1])
 
-#with open('../config.yaml') as file:
-#    config = yaml.load(file, Loader=SafeLoader)
-#
-#authenticator = stauth.Authenticate(
-#    config['credentials'],
-#    config['cookie']['name'],
-#    config['cookie']['key'],
-#    config['cookie']['expiry_days'],
-#    config['preauthorized']
-#)
-#name, authentication_status, username = authenticator.login('Login', 'main')
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+name, authentication_status, username = authenticator.login('Login', 'main')
 #
 #if authentication_status:
 #    authenticator.logout('Logout', 'main')
