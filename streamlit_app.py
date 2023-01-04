8dcb23dbf8d613590725f03961253c1c9b1ab9b5
Maxim <maxim.oweyssi@est.org.uk>
2023-01-04 16:20:09 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,14 +19,14 @@ authenticator = stauth.Authenticate(
 )
 name, authentication_status, username = authenticator.login('Login', 'main')
 
-if authentication_status:
-    authenticator.logout('Logout', 'main')
-    st.write(f'Welcome *{name}*')
-    st.title('Some content')
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
+#if authentication_status:
+#    authenticator.logout('Logout', 'main')
+#    st.write(f'Welcome *{name}*')
+#    st.title('Some content')
+#elif authentication_status == False:
+#    st.error('Username/password is incorrect')
+#elif authentication_status == None:
+#    st.warning('Please enter your username and password')
 
 #if st.session_state["authentication_status"]:
 #    authenticator.logout('Logout', 'main')
