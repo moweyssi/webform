0a4daa1b901ef07047b521400027a5d19691e4d4
Maxim <maxim.oweyssi@est.org.uk>
2023-01-04 17:02:05 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,34 +17,34 @@ authenticator = stauth.Authenticate(
     config['preauthorized']
 )
 name, authentication_status, username = authenticator.login('Login', 'main')
-#
-#if authentication_status:
-#    authenticator.logout('Logout', 'main')
-#    st.write(f'Welcome *{name}*')
-#    st.title('Some content')
-#elif authentication_status == False:
-#    st.error('Username/password is incorrect')
-#elif authentication_status == None:
-#    st.warning('Please enter your username and password')
 
-#if st.session_state["authentication_status"]:
-#    authenticator.logout('Logout', 'main')
-#    st.write(f'Welcome *{st.session_state["name"]}*')
-#    st.title('Some content')
-#elif st.session_state["authentication_status"] == False:
-#    st.error('Username/password is incorrect')
-#elif st.session_state["authentication_status"] == None:
-#    st.warning('Please enter your username and password')
-#
-#if authentication_status:
-#    try:
-#        if authenticator.reset_password(username, 'Reset password'):
-#            st.success('Password modified successfully')
-#    except Exception as e:
-#        st.error(e)
-#
-#try:
-#    if authenticator.register_user('Register user', preauthorization=False):
-#        st.success('User registered successfully')
-#except Exception as e:
-#    st.error(e)
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{name}*')
+    st.title('Some content')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
+
+if st.session_state["authentication_status"]:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] == False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] == None:
+    st.warning('Please enter your username and password')
+
+if authentication_status:
+    try:
+        if authenticator.reset_password(username, 'Reset password'):
+            st.success('Password modified successfully')
+    except Exception as e:
+        st.error(e)
+
+try:
+    if authenticator.register_user('Register user', preauthorization=False):
+        st.success('User registered successfully')
+except Exception as e:
+    st.error(e)
