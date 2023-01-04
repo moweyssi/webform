932eb2af36610c04f399092dd9d0661f892d2112
Maxim <maxim.oweyssi@est.org.uk>
2023-01-04 17:06:33 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,14 +27,14 @@ elif authentication_status == False:
 elif authentication_status == None:
     st.warning('Please enter your username and password')
 
-if st.session_state["authentication_status"]:
-    authenticator.logout('Logout', 'main')
-    st.write(f'Welcome *{st.session_state["name"]}*')
-    st.title('Some content')
-elif st.session_state["authentication_status"] == False:
-    st.error('Username/password is incorrect')
-elif st.session_state["authentication_status"] == None:
-    st.warning('Please enter your username and password')
+#if st.session_state["authentication_status"]:
+#    authenticator.logout('Logout', 'main')
+#    st.write(f'Welcome *{st.session_state["name"]}*')
+#    st.title('Some content')
+#elif st.session_state["authentication_status"] == False:
+#    st.error('Username/password is incorrect')
+#elif st.session_state["authentication_status"] == None:
+#    st.warning('Please enter your username and password')
 
 #if authentication_status:
 #    try:
